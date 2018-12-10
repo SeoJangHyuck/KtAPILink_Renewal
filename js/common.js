bc8ca45204511368474ab3334d5fca4341164352
SeoJangHyuck <sjh938606@gmail.com>
2018-12-10 17:10:54 +0900

    no message

--- js/common.js
+++ js/common.js
@@ -1,2 +1,10 @@
 $(function(){
+    $("#k_gnb>li").on("mouseover focusin",function(){
+        $(this).addClass("k_gnb_act");
+        $(this).children().addClass("k_gnb_act");
+    });
+    $("#k_gnb>li").on("mouseout focusout",function(){
+        $(this).removeClass("k_gnb_act");
+        $(this).children().removeClass("k_gnb_act");
+    });
 }); //END
