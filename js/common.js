202edca95ccf8d3334791e0ed2e67076425c6c6d
SeoJangHyuck <sjh938606@gmail.com>
2018-12-12 16:34:39 +0900

    no message

--- js/common.js
+++ js/common.js
@@ -7,4 +7,18 @@ $(function(){
         $(this).removeClass("k_gnb_act");
         $(this).children().removeClass("k_gnb_act");
     });
+    
+    var mcount = 0;
+    $(".m_k_gnb_btn").click(function(){
+        mcount++;
+        if(mcount%2==1){
+            $(this).addClass("m_k_gnb_act");
+            $(this).children().addClass("m_k_gnb_act");
+            $(this).children("span").css("transition-duration","0.4s");
+        }else{
+            $(this).removeClass("m_k_gnb_act");
+            $(this).children().removeClass("m_k_gnb_act");
+            $(this).children("span").css("transition-duration","0s");
+        };
+    });
 }); //END
